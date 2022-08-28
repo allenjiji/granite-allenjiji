4a126201ee17adf1bdf840bd414910fc6c56f563
Allen Jiji <allenjiji2604@gmail.com>
2022-08-28 19:14:34 +0530

    Added React.js to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,28 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// // This file is automatically compiled by Webpack, along with any other files
+// // present in this directory. You're encouraged to place your actual application logic in
+// // a relevant structure within app/javascript and only use these pack files to reference
+// // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "../stylesheets/application.scss"
+// import Rails from "@rails/ujs"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+// import "../stylesheets/application.scss"
 
 
-Rails.start()
-ActiveStorage.start()
+// Rails.start()
+// ActiveStorage.start()
+// // Support component names relative to this directory:
+// var componentRequireContext = require.context("components", true);
+// var ReactRailsUJS = require("react_ujs");
+// ReactRailsUJS.useContext(componentRequireContext);
+
+import "../stylesheets/application.scss";
+
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
+require("channels");
+
+var componentRequireContext = require.context("src", true);
+var ReactRailsUJS = require("react_ujs");
+
+ReactRailsUJS.useContext(componentRequireContext);
