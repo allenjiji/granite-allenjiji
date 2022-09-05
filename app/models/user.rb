832e61a278dfbfdc41ec1df64f4eb9b9c354b025
Allen Jiji <allenjiji2604@gmail.com>
2022-09-05 13:29:17 +0530

    Added task owner to tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
   MAX_EMAIL_LENGTH = 255
 
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
+  has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   has_secure_password
   has_secure_token :authentication_token
+  before_destroy :assign_tasks_to_task_owners
 
   validates :name, presence: true, length: { maximum: 35 }
   validates :email, presence: true,
@@ -22,5 +24,12 @@ class User < ApplicationRecord
 
     def to_lowercase
       email.downcase!
+    end
+
+    def assign_tasks_to_task_owners
+      tasks_whose_owner_is_not_current_user = assigned_tasks.select { |task| task.task_owner_id != id }
+      tasks_whose_owner_is_not_current_user.each do |task|
+        task.update(assigned_user_id: task.task_owner_id)
+      end
     end
 end
