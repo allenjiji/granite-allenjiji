f27618960e278496967db69a12a35c945239377b
Allen Jiji <allenjiji2604@gmail.com>
2022-09-09 19:34:25 +0530

    Added ability to send email to each user with pending tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :tasks, dependent: :destroy, foreign_key: :user_id
   has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
+  has_many :user_notifications, dependent: :destroy, foreign_key: :user_id
+  has_one :preference, dependent: :destroy, foreign_key: :user_id
 
   validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates :email,
@@ -21,6 +23,7 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lowercase
+  before_create :build_default_preference
 
   has_secure_password
   has_secure_token :authentication_token
@@ -29,5 +32,9 @@ class User < ApplicationRecord
 
     def to_lowercase
       email.downcase!
+    end
+
+    def build_default_preference
+      self.build_preference(notification_delivery_hour: Constants::DEFAULT_NOTIFICATION_DELIVERY_HOUR)
     end
 end
---
+++ db/migrate/20220909105339_add_default_preference_to_existing_users.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class AddDefaultPreferenceToExistingUsers < ActiveRecord::Migration[6.1]
+  def up
+    users_with_nil_preference = User.where.missing(:preference)
+    users_with_nil_preference.each do |user|
+      user.send(:build_default_preference)
+      user.password = "welcome"
+      user.password_confirmation = "welcome"
+      user.save!
+    end
+   end
+
+  def down
+    User.find_each do |user|
+      user.preference.delete
+    end
+  end
+end
