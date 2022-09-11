ee27725344719fa204641ddb2f14371349a72d2a
Allen Jiji <allenjiji2604@gmail.com>
2022-09-11 16:21:08 +0530

    Moved functionality from application controller to concerns

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,9 @@
 
 class TasksController < ApplicationController
   before_action :load_task!, only: %i[show update destroy]
+  before_action :ensure_authorized_update_to_restricted_attrs, only: :update
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
-  before_action :ensure_authorized_update_to_restricted_attrs, only: :update
 
   def index
     tasks = policy_scope(Task)
